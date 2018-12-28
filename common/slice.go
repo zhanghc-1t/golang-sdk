847d91d5ebfb619bf9125a2a6f336647e201d9f2
zhanghaocheng <zhanghaocheng@innotechx.com>
2018-12-28 18:14:42 +0800

    fix

--- common/slice.go
+++ common/slice.go
@@ -1,10 +1,11 @@
 package common
 
 import (
+	"fmt"
 	"reflect"
 )
 
-func in(needle interface{}, haystack interface{}) int32 {
+func in(needle interface{}, haystack interface{}) int {
 	nType := reflect.TypeOf(needle)
 	hType := reflect.TypeOf(haystack)
 	if hType.Kind() != reflect.Slice ||
@@ -16,9 +17,39 @@ func in(needle interface{}, haystack interface{}) int32 {
 	hValueLength := hValue.Len()
 	for i := 0; i < hValueLength; i++ {
 		if reflect.DeepEqual(hValue.Index(i).Interface(), needle) {
-			return int32(i)
+			return i
 		}
 	}
 
 	return -1
+}
+
+func column(haystack interface{}, column string) (interface{}, error) {
+	aType := reflect.TypeOf(haystack)
+	aTypeKind := aType.Kind()
+	if aTypeKind != reflect.Slice && aTypeKind != reflect.Map {
+		return nil, fmt.Errorf("slice/map get column error:haystack is not a slice or map")
+	}
+
+	return nil, nil
+}
+
+func slice_column(haystack []interface{}, column string) (interface{}, error) {
+	return nil, nil
+}
+
+func map_column(haystack []interface{}, column string) (interface{}, error) {
+	return nil, nil
+}
+
+func value(object interface{}, key string) (interface{}, error) {
+	return nil, nil
+}
+
+func map_value(object map[string]interface{}, key string) (interface{}, error) {
+	return nil, nil
+}
+
+func struct_value(object interface{}, key string) (interface{}, error) {
+	return nil, nil
 }
