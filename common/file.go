b00d2c6e54d4f342d242af88ba894655616a4dd4
zhanghaocheng <zhanghaocheng@innotechx.com>
2019-03-18 13:38:42 +0800

    fix

---
+++ common/file.go
@@ -0,0 +1,15 @@
+package common
+
+import "os"
+
+func FileExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	return false, err
+}
